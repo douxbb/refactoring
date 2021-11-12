2636adcb6abf3f27cae1afd9b27ee65243cb0f5c
Eugeniy Kuznetsov <eugeny.kuz@gmail.com>
2021-11-12 23:03:19 +0500

    Step 2

--- filter.py
+++ filter.py
@@ -1,28 +1,38 @@
 from PIL import Image
 import numpy as np
+
+
+def convert_image_to_pixelart(image, chunk_size=10, grey_gradation=5):
+    arr = np.array(image)
+    gradient = chunk_size * grey_gradation
+    width = len(arr)
+    height = len(arr[1])
+
+    for i in range(0, width - chunk_size + 1, chunk_size):
+        for j in range(0, height - chunk_size + 1, chunk_size):
+            grey = 0
+
+            for x in range(i, i + chunk_size):
+                for y in range(j, j + chunk_size):
+                    red = int(arr[x][y][0])
+                    green = int(arr[x][y][1])
+                    blue = int(arr[x][y][2])
+                    light = red + green + blue
+                    grey += light / 3
+
+            grey = int(grey // chunk_size ** 2)
+
+            for x in range(i, i + chunk_size):
+                for y in range(j, j + chunk_size):
+                    local_grey = int(grey // gradient) * gradient
+
+                    arr[x][y][0] = local_grey
+                    arr[x][y][1] = local_grey
+                    arr[x][y][2] = local_grey
+
+    return arr
+
+
 img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a - 9:
-    j = 0
-    while j < a1 - 9:
-        s = 0
-        for n in range(i, i + 10):
-            for y in range(j, j + 10):
-                n1 = int(arr[n][y][0])
-                n2 = int(arr[n][y][1])
-                n3 = int(arr[n][y][2])
-                M = n1 + n2 + n3
-                s += M / 3
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for y in range(j, j + 10):
-                arr[n][y][0] = int(s // 50) * 50
-                arr[n][y][1] = int(s // 50) * 50
-                arr[n][y][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
+res = Image.fromarray(convert_image_to_pixelart(img))
 res.save('res.jpg')
