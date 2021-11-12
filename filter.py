64eb83e8973d8fe6ca4773dae47fe84b86b38da9
AlexFaernon <alexpavlenko6666@rambler.ru>
2021-11-12 20:36:17 +0500

    2 этап

--- filter.py
+++ filter.py
@@ -1,28 +1,38 @@
 from PIL import Image
 import numpy as np
-img = Image.open("C:\\Users\Статик\Desktop\\refactoring\img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a:
-    j = 0
-    while j < a1:
-        s = 0
-        for n in range(i, i + 10):
-            for n0 in range(j, j + 10):
-                n1 = arr[n][n0][0]
-                n2 = arr[n][n0][1]
-                n3 = arr[n][n0][2]
-                M = (int(n1) + int(n2) + int(n3)) // 3
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
-res.save('C:\\Users\Статик\Desktop\\refactoring\\res.jpg')
+
+
+def get_average_rgb(local_x, local_y):
+    global img_matrix
+    n1 = img_matrix[local_x][local_y][0]
+    n2 = img_matrix[local_x][local_y][1]
+    n3 = img_matrix[local_x][local_y][2]
+    return (int(n1) + int(n2) + int(n3)) // 3
+
+
+CELL_X_OFFSET = 10
+CELL_Y_OFFSET = 10
+GREY_GRADATION = 50
+img = Image.open(
+    "C:\\Users\Статик\Desktop\\refactoring\img2.jpg")  # по какой-то причине не работает относительный адрес к картинке
+img_matrix = np.array(img)
+len_x = len(img_matrix)
+len_y = len(img_matrix[1])
+cell_x = 0
+while cell_x < len_x:
+    cell_y = 0
+    while cell_y < len_y:
+        sum_RGB = 0
+        for x in range(cell_x, cell_x + CELL_X_OFFSET):
+            for y in range(cell_y, cell_y + CELL_Y_OFFSET):
+                sum_RGB += get_average_rgb(x, y)
+        sum_RGB = int(sum_RGB // 100)
+        for x in range(cell_x, cell_x + CELL_X_OFFSET):
+            for y in range(cell_y, cell_y + CELL_Y_OFFSET):
+                img_matrix[x][y][0] = int(sum_RGB // GREY_GRADATION) * GREY_GRADATION
+                img_matrix[x][y][1] = int(sum_RGB // GREY_GRADATION) * GREY_GRADATION
+                img_matrix[x][y][2] = int(sum_RGB // GREY_GRADATION) * GREY_GRADATION
+        cell_y = cell_y + CELL_Y_OFFSET
+    cell_x = cell_x + CELL_X_OFFSET
+res = Image.fromarray(img_matrix)
+res.save('C:\\Users\Статик\Desktop\\refactoring\\res.jpg')  # то же самое что и в первом случае
