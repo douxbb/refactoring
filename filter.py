2df9222e901aa2159230333edcd04659284f5428
Eugeniy Kuznetsov <eugeny.kuz@gmail.com>
2021-11-13 01:13:43 +0500

    Step 3

--- filter.py
+++ filter.py
@@ -5,30 +5,13 @@ import numpy as np
 def convert_image_to_pixelart(image, chunk_size=10, grey_gradation=5):
     arr = np.array(image)
     gradient = chunk_size * grey_gradation
-    width = len(arr)
-    height = len(arr[1])
 
-    for i in range(0, width - chunk_size + 1, chunk_size):
-        for j in range(0, height - chunk_size + 1, chunk_size):
-            grey = 0
+    for x in range(0, arr.shape[0], chunk_size):
+        for y in range(0, arr.shape[1], chunk_size):
+            grey = sum(map(lambda n: int(n)/3, arr[x:x+chunk_size, y:y+chunk_size].flatten()))
+            grey = int(grey / chunk_size ** 2 // gradient) * gradient
 
-            for x in range(i, i + chunk_size):
-                for y in range(j, j + chunk_size):
-                    red = int(arr[x][y][0])
-                    green = int(arr[x][y][1])
-                    blue = int(arr[x][y][2])
-                    light = red + green + blue
-                    grey += light / 3
-
-            grey = int(grey // chunk_size ** 2)
-
-            for x in range(i, i + chunk_size):
-                for y in range(j, j + chunk_size):
-                    local_grey = int(grey // gradient) * gradient
-
-                    arr[x][y][0] = local_grey
-                    arr[x][y][1] = local_grey
-                    arr[x][y][2] = local_grey
+            arr[x:x+chunk_size, y:y+chunk_size] = grey
 
     return arr
 
