cd59dfba58b8d8c107c0983c529b7faef793bb57
ARTYOM ARAKELOV <artumarakelov@gmail.com>
2021-11-13 12:04:58 +0500

    part 3

--- filter.py
+++ filter.py
@@ -5,29 +5,12 @@ import numpy as np
 def convert_to_pixelart(image, chunk_size=10, grey_gradation=5):
     arr = np.array(image)
     gradient = chunk_size * grey_gradation
-    width = len(arr)
-    height = len(arr[1])
-    for i in range(0, width - chunk_size + 1, chunk_size):
-        for j in range(0, height - chunk_size + 1, chunk_size):
-            grey = count_grey(arr, chunk_size, i, j)
-            for x in range(i, i + chunk_size):
-                for y in range(j, j + chunk_size):
-                    local_grey = int(grey // gradient) * gradient
-                    arr[x][y][0] = local_grey
-                    arr[x][y][1] = local_grey
-                    arr[x][y][2] = local_grey
+    for x in range(0, arr.shape[0], chunk_size):
+        for y in range(0, arr.shape[1], chunk_size):
+            grey = sum(map(lambda n: int(n) / 3, arr[x:x + chunk_size, y:y + chunk_size].flatten()))
+            grey = int(grey / chunk_size ** 2 // gradient) * gradient
+            arr[x:x + chunk_size, y:y + chunk_size] = grey
     return arr
-
-
-def count_grey(arr, chunk_size, i, j):
-    grey = 0
-    for x in range(i, i + chunk_size):
-        for y in range(j, j + chunk_size):
-            red = int(arr[x][y][0])
-            green = int(arr[x][y][1])
-            blue = int(arr[x][y][2])
-            grey += (red + green + blue) / 3
-    return int(grey // chunk_size ** 2)
 
 
 img = Image.open("img2.jpg")
