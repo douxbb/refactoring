1a1fd2f6178a3fedc12e2f328cd832a4b328e5d7
AlexFaernon <alexpavlenko6666@rambler.ru>
2021-11-14 17:45:45 +0500

    3 этап

--- filter.py
+++ filter.py
@@ -1,15 +1,6 @@
 from PIL import Image
 import numpy as np
 
-
-def get_average_rgb(local_x, local_y):
-    global img_matrix
-    n1 = img_matrix[local_x][local_y][0]
-    n2 = img_matrix[local_x][local_y][1]
-    n3 = img_matrix[local_x][local_y][2]
-    return (int(n1) + int(n2) + int(n3)) // 3
-
-
 CELL_X_OFFSET = 10
 CELL_Y_OFFSET = 10
 GREY_GRADATION = 50
@@ -23,15 +14,11 @@ while cell_x < len_x:
     cell_y = 0
     while cell_y < len_y:
         sum_RGB = 0
-        for x in range(cell_x, cell_x + CELL_X_OFFSET):
-            for y in range(cell_y, cell_y + CELL_Y_OFFSET):
-                sum_RGB += get_average_rgb(x, y)
+        sum_RGB += np.sum(img_matrix[cell_x:cell_x + CELL_X_OFFSET, cell_y:cell_y + CELL_Y_OFFSET]) // 3
         sum_RGB = int(sum_RGB // 100)
-        for x in range(cell_x, cell_x + CELL_X_OFFSET):
-            for y in range(cell_y, cell_y + CELL_Y_OFFSET):
-                img_matrix[x][y][0] = int(sum_RGB // GREY_GRADATION) * GREY_GRADATION
-                img_matrix[x][y][1] = int(sum_RGB // GREY_GRADATION) * GREY_GRADATION
-                img_matrix[x][y][2] = int(sum_RGB // GREY_GRADATION) * GREY_GRADATION
+        grey_matrix = np.zeros((CELL_X_OFFSET, CELL_Y_OFFSET, 3))
+        grey_matrix[:] = int(sum_RGB // GREY_GRADATION) * GREY_GRADATION
+        img_matrix[cell_x:cell_x + CELL_X_OFFSET, cell_y:cell_y + CELL_Y_OFFSET] = grey_matrix
         cell_y = cell_y + CELL_Y_OFFSET
     cell_x = cell_x + CELL_X_OFFSET
 res = Image.fromarray(img_matrix)
