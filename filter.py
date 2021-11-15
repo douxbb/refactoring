3efb083b08d01924f7d7a00af34f82d4b309f5ff
DFjay <pezhikov@gmail.com>
2021-11-15 18:04:47 +0500

    2 этап

--- filter.py
+++ filter.py
@@ -1,29 +1,35 @@
 from PIL import Image
 import numpy as np
 
+
+def convert_img_to_pixel(image, size=10, gradation_step=5):
+    arr_img = np.array(image)
+    height = len(arr_img)
+    width = len(arr_img[1])
+    gradation = size * gradation_step
+    for i in range(0, height - 1, size):
+        for j in range(0, width - 1, size):
+            grey = get_grey(arr_img, i, j, size)
+            for x in range(i, i + size):
+                for y in range(j, j + size):
+                    grey_pixel = int(grey // gradation) * gradation / 3
+                    arr_img[x][y][0] = grey_pixel
+                    arr_img[x][y][1] = grey_pixel
+                    arr_img[x][y][2] = grey_pixel
+    return arr_img
+
+
+def get_grey(arr, i, j, size):
+    grey = 0
+    for x in range(i, i + size):
+        for y in range(j, j + size):
+            red = int(arr[x][y][0])
+            green = int(arr[x][y][1])
+            blue = int(arr[x][y][2])
+            grey += red + green + blue
+    return int(grey // (size * size))
+
+
 img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a - 1:
-    j = 0
-    while j < a1 - 1:
-        s = 0
-        for n in range(i, i + 10):
-            for m in range(j, j + 10):
-                n1 = int(arr[n][m][0])
-                n2 = int(arr[n][m][1])
-                n3 = int(arr[n][m][2])
-                M = n1 + n2 + n3
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for m in range(j, j + 10):
-                arr[n][m][0] = int(s // 50) * 50 / 3
-                arr[n][m][1] = int(s // 50) * 50 / 3
-                arr[n][m][2] = int(s // 50) * 50 / 3
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
+res = Image.fromarray(convert_img_to_pixel(img))
 res.save('res.jpg')
