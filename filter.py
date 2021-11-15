74313dbce8c6426399b520b8b236986d82af5092
Daniil <volkov.daniil.vit@ya.ru>
2021-11-15 12:59:09 +0500

    3 Этап

--- filter.py
+++ filter.py
@@ -12,18 +12,10 @@ i = 0
 while i < height:
     j = 0
     while j < width:
-        pixel = 0
-        for n in range(i, i + mosaic_size):
-            for n1 in range(j, j + mosaic_size):
-                r = int(arr[n][n1][0])
-                g = int(arr[n][n1][1])
-                b = int(arr[n][n1][2])
-                average = (r + g + b) / 3
-                pixel += average
-        pixel = pixel // (100 * grayscale) * grayscale
-        for n in range(i, i + mosaic_size):
-            for n1 in range(j, j + mosaic_size):
-                arr[n][n1] = pixel
+        pixel = arr[i: i + mosaic_size, j: j + mosaic_size]
+        pixel_color = (pixel.sum() / 3) // (100 * grayscale) * grayscale
+        new_pixel = np.full(300, pixel_color).reshape(10, 10, 3)
+        arr[i: i + mosaic_size, j: j + mosaic_size] = new_pixel
         j = j + mosaic_size
     i = i + mosaic_size
 
