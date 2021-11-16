0cc454adc176a909eeaf56d9dcdfc3a0eec96833
DFjay <pezhikov@gmail.com>
2021-11-16 16:41:37 +0500

    4 этап

--- filter.py
+++ filter.py
@@ -14,6 +14,8 @@ def convert_img_to_pixel(image, size=10, gradation_step=5):
     return arr_img
 
 
-img = Image.open("img2.jpg")
-res = Image.fromarray(convert_img_to_pixel(img))
-res.save('res.jpg')
+img = Image.open(input('Введите путь до файла с картинкой: '))
+img_size = int(input("Введите размер мозаики: ") or 10)
+gradation_img = int(input("Введите количество шагов градации: ") or 5)
+res = Image.fromarray(convert_img_to_pixel(img, img_size, gradation_img))
+res.save(input('Готово. Введите название оутпут файла: '))
