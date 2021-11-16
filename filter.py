f6162e3ce6cc5324fd3253d38791751c84d89fc2
DFjay <pezhikov@gmail.com>
2021-11-16 18:28:39 +0500

    4 этап. Исправил неверное отображение с размером мозаики 1, градацией 255

--- filter.py
+++ filter.py
@@ -6,7 +6,7 @@ def convert_img_to_pixel(image, size=10, gradation_step=5):
     arr_img = np.array(image)
     height = len(arr_img)
     width = len(arr_img[1])
-    gradation = size * gradation_step
+    gradation = 255 // gradation_step
     for x in range(0, height, size):
         for y in range(0, width, size):
             sum_pixel = sum(arr_img[y:y + size, x:x + size].flatten())
