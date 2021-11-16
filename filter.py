3a2cb386daa37bb7534420f3a2cee30227390882
idefant <inik3004@gmail.com>
2021-11-16 08:39:14 +0500

    step 2

--- filter.py
+++ filter.py
@@ -1,29 +1,45 @@
 from PIL import Image
 import numpy as np
 
-img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a - 1:
-    j = 0
-    while j < a1 - 1:
-        s = 0
-        for x in range(i, i + 10):
-            for y in range(j, j + 10):
-                n1 = arr[x][y][0] / 3
-                n2 = arr[x][y][1] / 3
-                n3 = arr[x][y][2] / 3
-                M = n1 + n2 + n3
-                s += M
-        s = int(s // 100)
-        for x in range(i, i + 10):
-            for y in range(j, j + 10):
-                arr[x][y][0] = int(s // 50) * 50
-                arr[x][y][1] = int(s // 50) * 50
-                arr[x][y][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
+
+def get_average_shade_gray(arr, i, j, pixel_size):
+    s = 0
+    for x in range(i, i + pixel_size):
+        for y in range(j, j + pixel_size):
+            n1 = arr[x][y][0] / 3
+            n2 = arr[x][y][1] / 3
+            n3 = arr[x][y][2] / 3
+            M = n1 + n2 + n3
+            s += M
+    return s
+
+
+def set_color(arr, i, j, s, pixel_size, gradation_step):
+    for x in range(i, i + pixel_size):
+        for y in range(j, j + pixel_size):
+            arr[x][y][0] = int(s // gradation_step) * gradation_step
+            arr[x][y][1] = int(s // gradation_step) * gradation_step
+            arr[x][y][2] = int(s // gradation_step) * gradation_step
+
+
+def image_to_pixelart(filename, pixel_size, gradation_step):
+    img = Image.open(filename)
+    arr = np.array(img)
+    height = len(arr)
+    width = len(arr[1])
+    i = 0
+    while i < height - 1:
+        j = 0
+        while j < width - 1:
+            s = get_average_shade_gray(arr, i, j, pixel_size)
+            s = int(s // pixel_size ** 2)
+            set_color(arr, i, j, s, pixel_size, gradation_step)
+            j = j + pixel_size
+        i = i + pixel_size
+    return arr
+
+
+arr = image_to_pixelart('img2.jpg', 10, 50)
+
 res = Image.fromarray(arr)
 res.save('res.jpg')
