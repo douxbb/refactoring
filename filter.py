93adc19cadae531f3b10b78d30a1e0d75cc10dec
sheka00 <72471472+sheka00@users.noreply.github.com>
2021-11-18 02:39:32 +0500

    1 этап

--- filter.py
+++ filter.py
@@ -1,28 +1,30 @@
 from PIL import Image
 import numpy as np
-img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
+img_input = "img2.jpg"
+img_output = 'res.jpg'
+img = Image.open(img_input)
+img_arr = np.array(img, dtype=np.uint32)
+img_width = len(img_arr)
+img_height = len(img_arr[1])
 i = 0
-while i < a - 11:
+while i < img_width:
     j = 0
-    while j < a1 - 11:
+    while j < img_height:
         s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                n1 = arr[n][n1][0]
-                n2 = arr[n][n1][1]
-                n3 = arr[n][n1][2]
-                M = n1 + n2 + n3
+        for x in range(i, i + 10):
+            for y in range(j, j + 10):
+                n1 = img_arr[x][y][0]
+                n2 = img_arr[x][y][1]
+                n3 = img_arr[x][y][2]
+                M = (n1 + n2 + n3) // 3
                 s += M
         s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
+        for x in range(i, i + 10):
+            for y in range(j, j + 10):
+                img_arr[x][y][0] = int(s // 50) * 50
+                img_arr[x][y][1] = int(s // 50) * 50
+                img_arr[x][y][2] = int(s // 50) * 50
         j = j + 10
     i = i + 10
-res = Image.fromarray(arr)
-res.save('res.jpg')
+res = Image.fromarray(img_arr.astype(np.uint8))
+res.save(img_output)
