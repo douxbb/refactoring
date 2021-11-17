2aca4ef4d950b982a29804c32fc15fbcc7bf9036
sheka00 <72471472+sheka00@users.noreply.github.com>
2021-11-17 23:35:11 +0500

    Update filter.py
    этап-2

--- filter.py
+++ filter.py
@@ -1,28 +1,58 @@
 from PIL import Image
 import numpy as np
-img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a - 11:
-    j = 0
-    while j < a1 - 11:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                n1 = arr[n][n1][0]
-                n2 = arr[n][n1][1]
-                n3 = arr[n][n1][2]
-                M = n1 + n2 + n3
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
-res.save('res.jpg')
+
+
+def get_greyscaled_pixels(step_width, mosaic_size, step_height, graid, img_arr, greyscale):
+    for x in range(step_width, step_width + mosaic_size):
+        for y in range(step_height, step_height + mosaic_size):
+            img_arr[x][y][0] = int(greyscale // graid) * graid
+            img_arr[x][y][1] = int(greyscale // graid) * graid
+            img_arr[x][y][2] = int(greyscale // graid) * graid
+    return img_arr
+
+
+def get_pixels_greyscale(step_width, mosaic_size, step_height, img_arr):
+    sum_pix = 0
+    for x in range(step_width, step_width + mosaic_size):
+        for y in range(step_height, step_height + mosaic_size):
+            r = img_arr[x][y][0]
+            g = img_arr[x][y][1]
+            b = img_arr[x][y][2]
+            avarage = (r + g + b)/3
+            sum_pix += avarage
+
+    return int(sum_pix // mosaic_size**2)
+
+
+def get_mosaic(graid, img_width, img_height, mosaic_size, img_arr):
+    step_width = 0
+    while step_width < img_width:
+        step_height = 0
+        while step_height < img_height:
+            greyscale = get_pixels_greyscale(step_width, mosaic_size, step_height, img_arr)
+
+            img_arr = get_greyscaled_pixels(step_width, mosaic_size, step_height, graid, img_arr, greyscale)
+
+            step_height += mosaic_size
+        step_width += mosaic_size
+
+    return img_arr
+
+
+def main():
+    img_input = "img2.jpg"
+    img_output = 'res.jpg'
+    img = Image.open(img_input)
+    img_arr = np.array(img, dtype=np.uint32)
+    img_width = len(img_arr)
+    img_height = len(img_arr[1])
+    mosaic_size = 10
+    step_gray = 5
+    gray = 255 / step_gray
+
+    mosaic = get_mosaic(gray, img_width, img_height, mosaic_size, img_arr)
+    res = Image.fromarray(mosaic.astype(np.uint8))
+    res.save(img_output)
+
+
+main()
